383471c8d8663d71e68a4699ed9f798455856daf
vacvvn <vacvvn2012@gmail.com>
2020-06-24 22:35:03 +0300

    add example code to files

---
+++ factorial.cpp
@@ -0,0 +1,8 @@
+nclude "functions.h"
+
+int factorial(int n){
+    if(n!=1){
+	return(n * factorial(n-1));
+    }
+    else return 1;
+}
---
+++ main.cpp
@@ -0,0 +1,11 @@
+nclude <iostream>
+#include "functions.h"
+
+using namespace std;
+
+int main(){
+    print_hello();
+    cout << endl;
+    cout << "The factorial of 5 is " << factorial(5) << endl;
+    return 0;
+}
